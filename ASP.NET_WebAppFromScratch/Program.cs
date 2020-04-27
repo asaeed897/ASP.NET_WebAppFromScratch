402ba75b66fa2a2671655d701b963b659dcafb42
awais <asaeed897>
2020-04-27 22:31:21 +0500

    Completed Lesson 10 Middleware Components

--- ASP.NET_WebAppFromScratch/Program.cs
+++ ASP.NET_WebAppFromScratch/Program.cs
@@ -22,6 +22,6 @@ namespace ASP.NET_WebAppFromScratch
                 {
                     webBuilder.UseStartup<Startup>();
                 });
-        // 9. AppSettings json file
+        // 10. Middleware Components
     }
 }
