6dc8d6cb311f3ac7939b3f6df62df87b49f0a59a
awais <asaeed897>
2020-04-30 22:07:37 +0500

    Completed Lesson 22 Customize View Discovery

--- ASP.NET_WebAppFromScratch/Program.cs
+++ ASP.NET_WebAppFromScratch/Program.cs
@@ -22,16 +22,21 @@ namespace ASP.NET_WebAppFromScratch
                 {
                     webBuilder.UseStartup<Startup>();
                 });
-        // 21. Views
+        // 22. Customize View Discovery
 
-        // A view file has cshtml extension
-        // A view is an HTML template with embedded Razor markup
-        // Contains logic to display Model data
-       //  Convention for adding views e.g. for HomeController>>> Views\Home\{method_name of View}e.g. for details it details.cshtml
+        // View() or View(Object model): Looks for a view file with the same name
+        // same name as the action method
+        // View(string viewName): 
+        // Looks for a view file with your own custom name: View("Test.cshtml")
+        // You can specify a View name or View file path View("MYViews/Test.cshtml")
+        // View file path can be absolute or relative
+        // With absolute path .cshtml extension must be specified("MYViews/Test.cshtml")
+        // with Relative path don't specify the file extension .cshtml: View("../MyVideo/Test")
 
-       
-        
 
-        
+
+
+
+
     }
 }
